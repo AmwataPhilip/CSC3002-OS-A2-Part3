b256525094260687b202c8972e47c8ac1b057958
Philip Amwata <philip@philipamwata.net>
2019-05-05 19:23:06 +0200

    Code complete, ordering of system output incorrect.

--- src/molecule/Carbon.java
+++ src/molecule/Carbon.java
@@ -1,28 +1,40 @@
 package molecule;
 
 public class Carbon extends Thread {
-	
-	private static int carbonCounter =0;
+
+	private static int carbonCounter = 0;
 	private int id;
 	private Methane sharedMethane;
-	
+
 	public Carbon(Methane methane_obj) {
-		Carbon.carbonCounter+=1;
-		id=carbonCounter;
+		Carbon.carbonCounter += 1;
+		id = carbonCounter;
 		this.sharedMethane = methane_obj;
 	}
-	
+
 	public void run() {
-	    try {	 
-	    	 // you will need to fix below
-	    	System.out.println("---Group ready for bonding---");	
-	    	sharedMethane.bond("C"+ this.id);  //bond
-	    	 
-	    	 
-	    	   	 
-	    }
-	    catch (InterruptedException ex) { /* not handling this  */}
-	   // System.out.println(" ");
+		try {
+			sharedMethane.mutex.acquire();
+			sharedMethane.addCarbon();
+			if (sharedMethane.getHydrogen() >= 4) {
+				sharedMethane.hydrogensQ.release(4);
+				sharedMethane.removeHydrogen(4);
+				sharedMethane.carbonQ.release();
+				sharedMethane.removeCarbon(1);
+			} else if (sharedMethane.getCarbon() == 1 && sharedMethane.getHydrogen() == 0) {
+				System.out.println("---Group ready for bonding---");
+				sharedMethane.mutex.release();
+			} else {
+				sharedMethane.mutex.release();
+			}
+			sharedMethane.carbonQ.acquire();
+			sharedMethane.bond("C" + this.id);
+			sharedMethane.barrier.b_wait();
+			sharedMethane.mutex.release();
+
+		} catch (InterruptedException ex) {
+			/* not handling this */}
+		// System.out.println(" ");
 	}
 
 }
--- src/molecule/Hydrogen.java
+++ src/molecule/Hydrogen.java
@@ -2,26 +2,38 @@ package molecule;
 
 public class Hydrogen extends Thread {
 
-	private static int carbonCounter =0;
+	private static int carbonCounter = 0;
 	private int id;
 	private Methane sharedMethane;
-	
-	
+
 	public Hydrogen(Methane methane_obj) {
-		Hydrogen.carbonCounter+=1;
-		id=carbonCounter;
+		Hydrogen.carbonCounter += 1;
+		id = carbonCounter;
 		this.sharedMethane = methane_obj;
-		
+
 	}
-	
+
 	public void run() {
-	    try {
-	    	 // you will need to fix below
-	    	System.out.println("---Group ready for bonding---");			 
-	    	sharedMethane.bond("H"+ this.id);
-	    }
-	   catch (InterruptedException ex) { /* not handling this  */}
-	    //System.out.println(" ");
+		try {
+			sharedMethane.mutex.acquire();
+			sharedMethane.addHydrogen();
+			if (sharedMethane.getHydrogen() >= 4 && sharedMethane.getCarbon() >= 1) {
+				sharedMethane.hydrogensQ.release(4);
+				sharedMethane.removeHydrogen(4);
+				sharedMethane.carbonQ.release();
+				sharedMethane.removeCarbon(1);
+			} else if (sharedMethane.getCarbon() == 0 && sharedMethane.getHydrogen() == 1) {
+				System.out.println("---Group ready for bonding---");
+				sharedMethane.mutex.release();
+			} else {
+				sharedMethane.mutex.release();
+			}
+			sharedMethane.hydrogensQ.acquire();
+			sharedMethane.bond("H" + this.id);
+			sharedMethane.barrier.b_wait();
+		} catch (InterruptedException ex) {
+			/* not handling this */}
+		// System.out.println(" ");
 	}
 
 }
